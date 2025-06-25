8b47e7a21017ebb82d254abbffdb55740ddd48db
joaco369 <joako.2villarreal@gmail.com>
2025-06-25 18:00:03 -0300

    Add AIService

---
+++ ChatApp/AIService.swift
@@ -0,0 +1,41 @@
+//
+//  AIService.swift
+//  ChatApp
+//
+//  Created by Joaquin Villarreal on 25/06/2025.
+//
+
+import Foundation
+
+class AIService {
+  
+  private let requestBuilder = RequestBuilder()
+  private let networkManager = NetworkManager()
+  private let errorMessage: String = "Error: Unable to generate AI Response"
+  
+  private let url = URL(string: "https://api.openai.com/v1/chat/completions")
+  
+  func getAiResponse(prompt: String) async -> String {
+    guard let request = requestBuilder.buildRequest(prompt: prompt, url: url) else {
+      print("Error. Failed to request API")
+      return errorMessage
+    }
+    
+    do {
+      let data = try await networkManager.sendRequest(request)
+      return decodeResponse(data)
+    } catch {
+      print("Error")
+      return errorMessage
+    }
+  }
+  
+  private func decodeResponse(_ data: Data) -> String {
+    do {
+      let aiResponse = try JSONDecoder().decode(AIResponse.self, from: data)
+    } catch {
+      print("Error. Failed to decode JSON")
+      return errorMessage
+    }
+  }
+}
